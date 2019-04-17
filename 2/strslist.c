336d994fc0221c2ef4d8f2388add7f97e45a9d9d
DQmacOS <deonquebec@gmail.com>
2019-04-17 19:57:14 +0900

    delete improve

--- 2/strslist.c
+++ 2/strslist.c
@@ -1,4 +1,4 @@
-#define DEBUG 1
+#define DEBUG 0
 #define log(x) if(DEBUG) printf(x)
 #define dbg if(DEBUG)
 #include <stdlib.h> // malloc
@@ -32,55 +32,24 @@ typedef struct
 // Prototype declarations
 
 LIST *createList( void);
-
-/* Deletes all data in list and recycles memory
-	return	NULL head pointer
-*/
 LIST *destroyList( LIST *pList);
-
 int addNode( LIST *pList, tTOKEN *dataInPtr);
-
-/* Removes data from list
-	return	0 not found
-			1 deleted
-*/
 int removeNode( LIST *pList, char *keyPtr, tTOKEN **dataOut);
-
-/* interface to search function
-	Argu	key being sought
-	dataOut	contains found data
-	return	1 successful
-			0 not found
-*/
 int searchList( LIST *pList, char *pArgu, tTOKEN **pDataOut);
-
-/* returns number of nodes in list
-*/
 int listCount( LIST *pList);
-
-/* returns	1 empty
-			0 list has data
-*/
 int emptyList( LIST *pList);
-
 //int fullList( LIST *pList);
-
 void printList( LIST *pList);
 static int _insert( LIST *pList, NODE *pPre, tTOKEN *dataInPtr);
-
-/* internal delete function
-	deletes data from a list and saves the (deleted) data to dataOut
-*/
 static void _delete( LIST *pList, NODE *pPre, NODE *pLoc, tTOKEN **dataOutPtr);
-
 static int _search( LIST *pList, NODE **pPre, NODE **pLoc, char *pArgu);
-
 tTOKEN *createToken( char *str);
-
-/* Deletes all data in token structure and recycles memory
-	return	NULL head pointer
-*/
 tTOKEN *destroyToken( tTOKEN *pToken);
+// What I created in addition
+NODE *createNode(void);
+NODE *destroyNode(NODE * node);
+
+
 
 ////////////////////////////////////////////////////////////////////////////////
 int main( void)
@@ -102,7 +71,7 @@ int main( void)
 
 	while(scanf( "%s", str) == 1)
 	{
-        printf("str is : %s\n", str);
+        dbg printf("str is : %s\n", str);
 
 		pToken = createToken(str);
         log("after create return main\n");
@@ -114,7 +83,7 @@ int main( void)
 			//destroyToken( pToken);
 	}
 	// print function call
-	//printList( list);
+	printList( list);
 
 	//destroyList( list);
 
@@ -131,6 +100,7 @@ LIST *createList( void){
         return NULL;
     }
     ptr->count = 0;
+    ptr->head = NULL;
     return ptr;
 }
 
@@ -144,8 +114,8 @@ tTOKEN *createToken( char *str){
         return NULL;
     }
     dbg printf("token created with %s\n", str);
-    ptr->freq=0;
-    ptr->token=strdup(str); // must free after usage.
+    ptr->freq=1;
+    ptr->token=strdup(str);
     return ptr;
 }
 
@@ -155,12 +125,18 @@ tTOKEN *createToken( char *str){
 			1 if duplicated key
 */
 int addNode( LIST *pList, tTOKEN *dataInPtr){
-    NODE *pPre=NULL, *pLoc=NULL;
+    NODE *pPre=NULL;
+    NODE *pLoc=NULL;
     int found = _search(pList, &pPre, &pLoc, dataInPtr->token);
+    dbg printf("_search end. pPre: %p, pLoc: %p\n", pPre, pLoc);
     dbg printf("found: %d\n", found);
     if(found){ // dupe key
+        pLoc->dataPtr->freq++;
+        destroyToken(dataInPtr);
         return 1;
     }
+
+    // pPre contains info about location
     int res = _insert(pList, pPre, dataInPtr);
     if(res == 0){ // overflow
         return -1;
@@ -179,26 +155,24 @@ int addNode( LIST *pList, tTOKEN *dataInPtr){
 static int _search( LIST *pList, NODE **pPre, NODE **pLoc, char *pArgu){
     *pPre = NULL;
     *pLoc = pList->head;
-    while(*pLoc != NULL && strcmp(
-                (*pLoc)->dataPtr->token,
-                pArgu
-                ) < 0){ // proceed until token is not less than argu.
-
+    while(
+            (*pLoc != NULL) &&
+            (strcmp((*pLoc)->dataPtr->token, pArgu) < 0)
+            ){ // proceed until token is not less than argu.
         *pPre = *pLoc;
         *pLoc = (*pLoc)->link;
     }
 
-    int found;
-    if(*pLoc == NULL){ // cannot be found.
-        found = 0;
-    }else if(strcmp(
-            (*pLoc)->dataPtr->token,
-            pArgu
-            ) == 0){ // token is the same as the ARgu.
+    // pPre < token < pLoc
+    // *pPre == NULL if the node with a new token must be the new head.
+
+    int found = 0;
+    if((*pLoc != NULL) &&
+            (strcmp((*pLoc)->dataPtr->token, pArgu) == 0)
+            ){ // token is the same as the ARgu.
             found = 1;
-    }else{ // token is larger than Argu.
-        found = 0;
     }
+    log("_search returning...");
     return found;
 }
 
@@ -213,10 +187,11 @@ static int _insert( LIST *pList, NODE *pPre, tTOKEN *dataInPtr){
     if(pNew == NULL){
         return 0;
     }
+
     pNew->dataPtr = dataInPtr;
 
     if(pPre == NULL){ // pNew must be the new head
-        pNew->link = pList->head;
+        pNew->link = pList->head; // NULL
         pList->head = pNew;
     }else{
         pNew->link = pPre->link;
@@ -224,6 +199,7 @@ static int _insert( LIST *pList, NODE *pPre, tTOKEN *dataInPtr){
     }
     pList->count++;
     return 1;
+    log("_insert end\n");
 }
 
 /* prints data from list
@@ -231,8 +207,116 @@ static int _insert( LIST *pList, NODE *pPre, tTOKEN *dataInPtr){
 void printList( LIST *pList){
     NODE* curr = pList->head;
     while(curr){
-        printf("%s\t%d\n",curr->dataPtr->token, curr->dataPtr->freq);
+        printf("%s %d\n",curr->dataPtr->token, curr->dataPtr->freq);
+        printf("\n");
         curr = curr->link;
     }
 }
 
+/* returns	1 empty
+			0 list has data
+*/
+int emptyList( LIST *pList){
+    if(pList->count == 0){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+/* returns number of nodes in list
+*/
+int listCount( LIST *pList){
+    return pList->count;
+}
+
+/* Deletes all data in token structure and recycles memory
+	return	NULL head pointer
+*/
+tTOKEN *destroyToken( tTOKEN *pToken){
+    free(pToken->token);
+    pToken->token = NULL;
+    pToken->freq = 0;
+    free(pToken);
+    return NULL;
+}
+
+/* interface to search function
+	Argu	key being sought
+	dataOut	contains found data
+	return	1 successful
+			0 not found
+*/
+int searchList( LIST *pList, char *pArgu, tTOKEN **pDataOut){
+    NODE *pPre = NULL;
+    NODE *pLoc = NULL;
+    int found = _search(pList, &pPre, &pLoc, pArgu);
+    if(found){
+        *pDataOut = pLoc->dataPtr;
+    }else{
+        *pDataOut = NULL;
+    }
+    return found;
+}
+
+/* Removes data from list
+	return	0 not found
+			1 deleted
+*/
+int removeNode( LIST *pList, char *keyPtr, tTOKEN **dataOut){
+    NODE *pPre = NULL;
+    NODE *pLoc = NULL;
+    int found = _search(pList, &pPre, &pLoc, keyPtr);
+    if(found){
+        _delete(pList, pPre, pLoc, dataOut);
+        return 1;
+    }
+    return 0;
+}
+
+/* internal delete function
+	deletes data from a list and saves the (deleted) data to dataOut
+*/
+static void _delete( LIST *pList, NODE *pPre, NODE *pLoc, tTOKEN **dataOutPtr){
+    *dataOutPtr = pLoc->dataPtr;
+    if(pPre == NULL){
+        pList->head = pLoc->link;
+    }else{
+        pPre->link = pLoc->link;
+    }
+    destroyNode(pLoc);
+    pList->count--;
+}
+
+/* Deletes all data in list and recycles memory
+	return	NULL head pointer
+*/
+LIST *destroyList( LIST *pList){
+    NODE *delp;
+    while(pList->head != NULL){
+        delp = pList->head;
+        pList->head = pList->head->link;
+        destroyNode(delp);
+    }
+    pList->pos = NULL;
+    pList->count = 0;
+    free(pList);
+    return NULL;
+}
+
+NODE *destroyNode(NODE * node){
+    destroyToken(node->dataPtr);
+    node->link = NULL;
+    free(node);
+    return NULL;
+}
+NODE *createNode(void){
+    NODE *ptr = malloc(sizeof *ptr);
+    if(!ptr){
+        fprintf(stderr, "FATAL: malloc fail(createNode).");
+        abort();
+    }
+    ptr->dataPtr = NULL;
+    ptr->link = NULL;
+    return ptr;
+}
